c65b11ae9e32071d94dc086c501d06d30677037a
vishalsavaliya <vishandsavaliya@gmail.com>
2022-10-05 09:32:53 +0530

    working

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 import express from "express";
 import { config } from "dotenv";
-import paymentRoute from "./routes/paymentRouter.js"
-import cors from "cors"
-// var cors = require('cors')
-
-config({ path: "./config/config.env" })
-
-const corsOptions = {
-    origin: 'http://localhost:4000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
+import paymentRouter from "./routes/paymentRouter.js";
+import cors from "cors";
+config({ path: "./config/config.env" });
 
 export const app = express();
-app.use(cors(corsOptions));
-app.use("/api", paymentRoute)
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", paymentRouter);
+
 app.get("/api/getkey", (req, res) =>
     res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
 );
--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { checkout } from "../controllers/paymentControler.js";
+import { checkout, payment } from "../controllers/paymentControler.js";
 
 const router = express.Router();
 
 router.route("/checkout").post(checkout);
+router.route("/paymentverification").post(payment);
 
 export default router;
